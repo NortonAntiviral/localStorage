b459c395313b5775b06c939fa4a12a4b63d408a9
OfficialAntiviral <officialantiviral@gmail.com>
2019-01-27 14:57:16 -0500

    added local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,9 @@ function eventListeners() {
 
     // Remove tweet from the list
     tweetList.addEventListener('click', removeTweet);
+
+    // Document
+    document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 }
 
 
@@ -38,9 +41,12 @@ function newTweet(e) {
     // Add list
     tweetList.appendChild(li);
 
-    addTweetLocalStorage();
+    addTweetLocalStorage(tweet);
 
+    // Print the alert
+    alert('Tweet Added!!');
     // tweet.reset();
+    this.reset();
 }
 
 // Remove Tweet From DOM
@@ -48,9 +54,76 @@ function removeTweet(e) {
     if (e.target.classList.contains('remove-tweet')) {
         e.target.parentElement.remove();
     }
+
+    // Remove from Storage from Local Storage
+    removeTweetLocalStorage();
 }
 
 // Adds the tweets to local storage
 function addTweetLocalStorage(tweet) {
-    console.log('Hello from the Other Side');
+    let tweets = getTweetsFromStorage(e.target.parentElement.textContent);
+
+    // Add the tweet into array
+    tweets.push(tweet);
+
+    // Convert tweet array into String
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+
+}
+
+function getTweetsFromStorage() {
+    let tweets;
+    const tweetsLS = localStorage.getItem('tweets');
+    //Get values, if null is returned then we create an empty array
+    if (tweetsLS === null) {
+        tweets = [];
+    } else {
+        tweets = JSON.parse(tweetsLS);
+    }
+    return tweets;
+}
+
+// Prints Local Storage on Load
+function localStorageOnLoad() {
+    let tweets = getTweetsFromStorage();
+
+    // Loop through storage and then print the value
+    tweets.forEach(function (tweet) {
+        // Create remove button
+        const removeBtn = document.createElement('a');
+        removeBtn.classList = 'remove-tweet';
+        removeBtn.textContent = 'X';
+        // Create li
+        const li = document.createElement('li');
+        li.textContent = tweet;
+        tweetList.appendChild(li);
+
+        // Add Remove button to each tweet
+        li.appendChild(removeBtn);
+
+        // Add list
+        tweetList.appendChild(li);
+    });
+
+}
+
+//Removes tweet from Local Storage
+
+function removeTweetLocalStorage(tweet) {
+    //get tweets from storage
+    let tweets = getTweetsFromStorage();
+
+    //Remove X from tweet
+
+    const tweetDelete = tweet.substring(0, tweet.length - 1);
+
+    // Loop Throught the tweets  and remove the tweet that's equal
+    tweets.forEach(function (tweetsLS, index) {
+        if (tweetDelete === tweetsLS) {
+            tweets.splice(index, 1);
+        }
+    });
+
+    // Save the data
+    localStorage.setItem('tweets', JSON.stringify(tweets));
 }
